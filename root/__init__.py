0d22d76353058612b106ea2a7266a96d7f93579f
Kristaps Rumpis <kristapsrumpis.kr@gmail.com>
2026-03-16 10:57:22 +0000

    Add user loader function, conected login form to login route

--- root/__init__.py
+++ root/__init__.py
@@ -1,25 +1,23 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_login  import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
-
-# create flask app 
+# create flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# create db 
+# create db
 db = SQLAlchemy()
 
 # initialize flask login menager
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"  
 
 
 # importe routes
-from root.routes import home_bp
-
+from root.routes import home_bp  
 
 # register routes
 app.register_blueprint(home_bp)
@@ -29,4 +27,12 @@ db.init_app(app)
 
 
 # migrations db
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
+
+
+# import user model from models for user loader function\
+
+# user loader function
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- root/routes.py
+++ root/routes.py
@@ -1,58 +1,64 @@
-from flask import Blueprint, render_template,request,redirect, url_for
-from root.modules import accountCredentials
+from flask import Blueprint, redirect, render_template, request, url_for
+
 from root import db
+from root.modules import accountCredentials
 
-home_bp = Blueprint('home', __name__)
+home_bp = Blueprint("home", __name__)
 
-@home_bp.route('/', methods=['GET'])
+
+@home_bp.route("/", methods=["GET"])
 def home():
-    title = 'ACC Credentials'
+    title = "ACC Credentials"
     data = accountCredentials.query.all()
-    return render_template('home.html', title=title, data=data)
+    return render_template("home.html", title=title, data=data)
 
 
-@home_bp.route('/add-credenials', methods=['POST', 'GET'])
+@home_bp.route("/add-credenials", methods=["POST", "GET"])
 def add_credentials():
-    title = 'ADD Credentials'
+    title = "ADD Credentials"
     if request.method == "POST":
-        email = request.form['email']
-        account = request.form['account']
-        password = request.form['password']
-        accCredentials = accountCredentials(email=email, account=account,pasword=password )
+        email = request.form["email"]
+        account = request.form["account"]
+        password = request.form["password"]
+        accCredentials = accountCredentials(email=email,  account=account,  pasword=password)
+
         db.session.add(accCredentials)
         db.session.commit()
-        return redirect('/')
+        return redirect("/")
     else:
-        return render_template('add-account.html', title=title)
-        
+        return render_template("add-account.html", title=title)
 
-@home_bp.route('/delete-record/<int:id>', methods=['POST'])
+
+@home_bp.route("/delete-record/<int:id>", methods=["POST"])
 def delete_record(id):
-    if request.method == 'POST':
+    if request.method == "POST":
         credential = accountCredentials.query.get_or_404(id)
         db.session.delete(credential)
         db.session.commit()
-        return redirect('/')
+        return redirect("/")
     else:
-        return redirect('/')
-    
+        return redirect("/")
 
-@home_bp.route('/edite-record/<int:id>', methods=['GET','POST'])
+
+@home_bp.route("/edite-record/<int:id>", methods=["GET", "POST"])
 def edite_record(id):
-    title = 'EDITE RECORD'
+    title = "EDITE RECORD"
     credential = accountCredentials.query.get_or_404(id)
-    if request.method == 'POST':
-        credential.mail = request.form['email']
-        credential.account = request.form['account']
-        credential.pasword = request.form['password']
+    if request.method == "POST":
+        credential.mail = request.form["email"]
+        credential.account = request.form["account"]
+        credential.pasword = request.form["password"]
         db.session.commit()
-        return redirect('/')
-    
-    return render_template('edite.html', title=title, credential=credential)
+        return redirect("/")
+
+    return render_template("edite.html", title=title, credential=credential)
 
 
-@home_bp.route('/login', methods=['GET', 'POST'])
+@home_bp.route("/login", methods=["GET", "POST"])
 def loging():
     title = "Log In"
-    return render_template('login.html', title=title)
-   
+    if request.method == "POST":
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        print(email,password1)
+    return render_template("login.html", title=title)
